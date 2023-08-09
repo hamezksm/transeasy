6598ab8fb3437355a3fd7565e06edf4f872d3019
ElNgatia <ngatiadyte@gmail.com>
2023-08-09 13:22:03 +0300

    Update ui

--- lib/view/component/home_screen.dart
+++ lib/view/component/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:trans_e/view/widgets/search_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,16 +10,41 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(-1.283825, 36.824587);
+
+  void onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  SafeArea(
+    return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.white,
+          backgroundColor: Colors.grey,
           centerTitle: true,
           title: const Text('Trans-E'),
         ),
-        body: Container(),
+        body: Stack(
+          children: [
+            const SearchWidget(),
+            GoogleMap(
+              onMapCreated: onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 15,
+              ),
+              markers: {
+                const Marker(
+                  markerId: MarkerId('South C'),
+                  position: LatLng(-1.283825, 36.824587),
+                )
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
